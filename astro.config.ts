7d489a66a0cef5b00dbb576373319a5ac64c035e
Ubuntu <azureuser@base-ds1.uwv2cu3zcovu3p2ujvlbter0pa.ax.internal.cloudapp.net>
2023-06-26 16:54:17 +0000

    Fixed site name configuration

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,7 @@ import prefetch from "@astrojs/prefetch";
 // https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://mblkhr.github.io/blog",
+	site: "https://mblkhr.github.io/",
 	base: "/astro-blog-cactus",
 	markdown: {
 		shikiConfig: {
