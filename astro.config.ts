60a25ef30618dbb2fee0743d294f3d2f7274275d
Mohamed <91762637+mblkhr@users.noreply.github.com>
2023-07-06 09:43:44 +0000

    Fixed react dependancy

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
+import react from "react";
 
 // https://astro.build/config
 export default defineConfig({
